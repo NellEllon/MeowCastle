ce2675e8a898d25e96c3972f33ab234fd831f1f2
Winn <77405392+NellEllon@users.noreply.github.com>
2021-11-06 14:17:57 +0700

    Add files via upload

---
+++ Credit.java
@@ -0,0 +1,52 @@
+
+import java.awt.Color;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Credit extends JFrame
+{
+    public JFrame getF()
+    {   
+        return this;
+    }
+   private MyImageIcon credit =  new MyImageIcon("resources/CREDIT.png").resize(800,600);
+   private JPanel creditpane ;
+   public Credit(int x ,int y,int bg,int m)
+   {
+        setTitle("Credit");
+        setBounds(x, y, 800, 600);
+        setResizable(false);
+        setVisible(true);
+        creditpane = (JPanel) getContentPane();
+        setDefaultCloseOperation( WindowConstants.DO_NOTHING_ON_CLOSE);
+        JButton button1  = new JButton(new MyImageIcon("resources/BACKYELLOW1.png").resize(175, 175));
+        
+        button1.setBounds(-20,-50, 250, 200);
+        button1.setBackground(new Color(0,0,0,0));
+        button1.setOpaque(false);
+        button1.setBorder(BorderFactory.createLineBorder(new Color(0,0,0,0), 1));
+        button1.setFocusable(false);
+        button1.setContentAreaFilled(false);
+        button1.addActionListener( new ActionListener(){
+            public void actionPerformed( ActionEvent e ){          
+                new MainApplication(getF().getX(),getF().getY(),bg,m);              
+                dispose();  
+            }
+        });
+        button1.addMouseListener( new MouseAdapter() {
+            public void mousePressed( MouseEvent e ){
+                button1.setIcon(new MyImageIcon("resources/BACKYELLOW2.png").resize(175, 175));
+                button1.validate();            
+            }
+            public void mouseReleased( MouseEvent e )
+            {
+                button1.setIcon(new MyImageIcon("resources/BACKYELLOW1.png").resize(175, 175));
+                button1.validate();   
+            }
+        });
+        JLabel label = new JLabel(credit);
+        creditpane.add(button1);        
+        creditpane.add(label);
+        validate();
+    }
+}
